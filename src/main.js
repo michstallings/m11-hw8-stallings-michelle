ceb35550e9b8bf81c0eb3c0e86fd33f40197fb19
michstallings <contact@michellestallings.com>
2018-11-04 10:47:47 -0800

    Added method includeSpoon

--- src/main.js
+++ src/main.js
@@ -11,4 +11,10 @@ class IceCream extends Desserts {
 		this.flavor = flavor;
 		this.scoops = scoops;
 	}
+
+	includeSpoon() {
+		if(this.scoops === 3) {
+			console.log('Here is your spoon!');
+		}
+	}
 }
