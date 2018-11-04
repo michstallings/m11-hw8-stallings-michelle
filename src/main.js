45ce9249450fd3674f9e09bce1ce53ef78da1f17
michstallings <contact@michellestallings.com>
2018-11-04 11:03:54 -0800

    Added final statement.

--- src/main.js
+++ src/main.js
@@ -23,3 +23,7 @@ const vanillaIceCream = new IceCream('Ice Cream', 340, 'Vanilla', 3);
 
 console.log(vanillaIceCream);
 vanillaIceCream.includeSpoon();
+
+let ourStatement = I like + `${vanillaIceCream.flavor}`;
+
+console.log(ourStatement);
