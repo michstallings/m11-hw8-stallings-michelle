bc0cf4d432a3d933b747ac82b6b4a5cb8d85c309
michstallings <contact@michellestallings.com>
2018-11-04 10:43:05 -0800

    Added subclass IceCream

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 class Desserts {
 	constructor (type, calories){
+		this.type = type;
+		this.calories = calories;
+	}
+}
+
+class IceCream extends Desserts {
+	constructor(type, calories, flavor, scoops){
 
 	}
 }
